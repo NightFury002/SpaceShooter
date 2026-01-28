caaa863c34a3e224688bf738767dbae6b6f04a74
NightFury002 <phucthoc205@gmail.com>
2026-01-28 14:16:30 +0700

    update bullet shooting frome fighter's nose

--- Assets/PlayerShootingg.cs
+++ Assets/PlayerShootingg.cs
@@ -4,10 +4,21 @@ public class PlayerShootingg : MonoBehaviour
 {
     public GameObject bulletPrefabs;
     public float shootingInterval;
+    public Vector3 bulletOffset;
+
     private float lastBulletTime;
+
     void Update()
     {
-        if (Time.time - lastBulletTime >= shootingInterval)
+        if (Input.GetMouseButton(0))
+        {
+            UpdateFiring();
+        }
+    }
+
+    private void UpdateFiring()
+    {
+        if (Time.time - lastBulletTime > shootingInterval)
         {
             ShootBullet();
             lastBulletTime = Time.time;
@@ -16,6 +27,6 @@ public class PlayerShootingg : MonoBehaviour
 
     private void ShootBullet()
     {
-        Instantiate(bulletPrefabs, transform.position, transform.rotation);
+        var bullet = Instantiate(bulletPrefabs, transform.position + bulletOffset, transform.rotation);
     }
 }
