9a2882ec96f07c3870d42b9bde98921c0e3ee9b2
NightFury002 <phucthoc205@gmail.com>
2026-01-29 17:50:30 +0700

    Update ExplosionAnimation for enemy and update EnemyHealth

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -4,5 +4,12 @@ public class EnemyHealth : MonoBehaviour
 {
     public GameObject explosionPrefab;
     private void OnTriggerEnter2D(Collider2D collision) => Die();
-    private void Die() => Destroy(gameObject);
+
+    private void Die()
+    {
+        var explosion = Instantiate(explosionPrefab, transform.position,
+        transform.rotation);
+        Destroy(explosion, 1);
+        Destroy(gameObject);
+    }
 }
